20faeef10556c7ef292e7d8ad463ec6b4c3a484a
rites <Ritesh.awachat49@gmail.com>
2024-10-27 14:41:55 +0530

    Add this code

--- src/Sept24/_04092024/Day_1.java
+++ src/Sept24/_04092024/Day_1.java
@@ -1,9 +1,14 @@
 package Sept24._04092024;
 
 public class Day_1 {
+
     public static void main(String[] args) {
 
+        int a = 10;
+        int b = 22;
+        int c = (a+b);
 
+        System.out.println(c);
 
     }
 
---
+++ src/Sept24/_04092024/Task.java
@@ -0,0 +1,13 @@
+package Sept24._04092024;
+
+import java.net.StandardSocketOptions;
+
+public class Task {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+            }
+
+}
